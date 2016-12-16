8242838f921f6f69ff4a45cfa398b26b5d459383
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-12-16 18:58:54 +0100

    Removed parallel deflation

--- negentropy_multy.m
+++ negentropy_multy.m
@@ -2,15 +2,6 @@ function y = negentropy_multy(W, X)
     G = @(x) log(cosh(x));
     %G = @(x) -exp((-x.^2)./2);
     y = 0;
-    n = size(W,1);
-    
-    %Parallel deflation
-    WxWT = inv(W * W');
-    [F D] = eig(WxWT);
-    Dinv = inv(D);
-    Dinvsquare = sqrt(Dinv);
-    WxWTsquare = F*Dinvsquare*F';
-    W = WxWTsquare*W;
    
     %Optimization
     for i=1:n
