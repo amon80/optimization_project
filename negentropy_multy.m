18c39516530ad9cc2c1bfb867990cfcbebe60977
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-12-18 17:47:46 +0100

    Changed return variable name

--- negentropy_multy.m
+++ negentropy_multy.m
@@ -1,10 +1,10 @@
-function y = negentropy_multy(W, X)
+function f = negentropy_multy(W, X)
     G = @(x) log(cosh(x));
     %G = @(x) -exp((-x.^2)./2);
-    y = 0;
+    f = 0;
    
     %Optimization
     for i=1:n
-        y = y - mean(G(W(i, :) *  X));
+        f = f - mean(G(W(i, :) *  X));
     end
 end
